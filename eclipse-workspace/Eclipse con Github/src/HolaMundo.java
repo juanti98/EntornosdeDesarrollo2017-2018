3534fec7172069b5d329b99e27b36dd74209b76d
juanti98 <33227683+juanti98@users.noreply.github.com>
2017-12-10 22:08:17 +0100

    Update HolaMundo.java
    Esto se hizo justo despues del inicio

--- eclipse-workspace/Eclipse con Github/src/HolaMundo.java
+++ eclipse-workspace/Eclipse con Github/src/HolaMundo.java
@@ -2,5 +2,6 @@
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo!");
+		System.out.println("Este comentario lo he añadido despues del Hola Mundo!");
 	}
 }
